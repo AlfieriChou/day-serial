cc4ff37fa00476fe2da573919e9386d6c8ec8ed7
AlfieriChou <alfierichou@gmail.com>
2018-07-10 18:38:48 +0800

    MIT&&v1.1.2

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict'
 
 function serial (head) {
-  let timeStr = ''
   let result
   let reg = /^\d$/
   let now = new Date()
   let mouth = reg.test(now.getMonth() + 1) ? '0' + now.getMonth() + 1 : now.getMonth() + 1
   let day = reg.test(now.getDate()) ? '0' + now.getDate() : now.getDate()
-  timeStr += now.getFullYear() + mouth + day
+  let timeStr = now.getFullYear() + mouth + day
   return result = typeof (head) === 'undefined' ? 'A0000' + timeStr : head + timeStr
 }
 
